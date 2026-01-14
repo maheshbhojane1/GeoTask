4a9547efb6b93667736dbb36100d0b4e62764f09
Mahesh Bhojane <maheshbhojane200@gmail.com>
2026-01-14 23:18:37 +0530

    update style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../services/firebase";
 import { useNavigate } from "react-router-dom";
+import "../assets/location-dot.svg";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -17,13 +18,22 @@ export default function Login() {
   };
 
   return (
+    <>
+    
     <div className="container">
+      <div className="logo">
+        <div className="logo-icon"> <img src="location-dot.svg" alt="" /> </div>
+        <h1>GeoTask</h1>
+      </div>
       <div className="card">
-        <h2>Login to GeoTask</h2>
+        <h2>Welcome back</h2>
+        <p className="subtitle">Sign in to continue to your dashboard</p>
         <button className="btn" onClick={handleLogin}>
-          Login with Google
+         Sign in with Google
         </button>
       </div>
     </div>
+
+    </>
   );
 }
